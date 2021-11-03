5b912658cb0a794fa56585b4866df9b3a57e1738
Michael Trindade <michaeltrindadedasilva@gmail.com>
2021-11-02 22:41:21 -0400

    Componentes staticos

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,22 @@
+import DataTable from "components/DataTable";
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+
 function App() {
   return (
-    <div>
-      <h1 className="text-primary">Olá mundo!</h1>
-    </div>
+    <>
+      {/**abaixo importando um componente para dentro de outro componente, 
+     * que depois será renderizado no index.jsx 
+     * lembrando que no retorno da função no react, só pode ter 1 elemento, 
+     * para delimitar como 1 elemento só, usa-se o fragment <> </>
+    */}
+      <NavBar />
+      <div className="container">
+        <h1 className="text-primary">Olá mundo!</h1>
+        <DataTable/>
+      </div>
+      <Footer/>
+    </>
   );
 }
 
