f5a4e7fef12f06c6a7adc24342b45aed00f0d778
Michael Trindade <michaeltrindadedasilva@gmail.com>
2021-11-04 23:07:35 -0400

    Layers

---
+++ backend/src/main/java/com/mtstechnologies/mtsvendas/repositories/SellerRepository.java
@@ -0,0 +1,9 @@
+package com.mtstechnologies.mtsvendas.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.mtstechnologies.mtsvendas.entities.Seller;
+
+public interface SellerRepository extends JpaRepository<Seller, Long>{
+
+}
