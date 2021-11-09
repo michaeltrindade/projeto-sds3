7794b3b679eff939c5c100b82e201be441ed97d4
Michael Trindade <michaeltrindadedasilva@gmail.com>
2021-11-08 22:15:10 -0400

    Configurando Rotas

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,37 +1,9 @@
-import BarChart from "components/BarChart";
-import DataTable from "components/DataTable";
-import DonutChart from "components/DonutChart";
-import Footer from "components/Footer";
-import NavBar from "components/NavBar";
+import Routes from "Routes";
+
 
 function App() {
   return (
-    <>
-      {/**abaixo importando um componente para dentro de outro componente, 
-     * que depois será renderizado no index.jsx 
-     * lembrando que no retorno da função no react, só pode ter 1 elemento, 
-     * para delimitar como 1 elemento só, usa-se o fragment <> </>
-    */}
-      <NavBar />
-      <div className="container">
-        <h1 className="text-primary py-3">Dashboard de vendas</h1>
-        <div className="row px-3">
-          <div className="col-sm-6">
-            <h5 className="text-center text-secondary">Taxa de sucesso (%)</h5>
-            <BarChart />
-          </div>
-          <div className="col-sm-6">
-            <h5 className="text-center text-secondary">Taxa de sucesso (%)</h5>
-            <DonutChart />
-          </div>
-        </div>
-        <div className="py-3">
-          <h2 className="text-primary">Todas as vendas</h2>
-        </div>
-        <DataTable />
-      </div>
-      <Footer />
-    </>
+    <Routes />
   );
 }
 
