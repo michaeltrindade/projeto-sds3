42261d69ef801efef39323d5ceb364410b413c75
Michael Trindade <michaeltrindadedasilva@gmail.com>
2021-11-10 23:56:34 -0400

    Configuração variavel de ambiente

--- frontend/src/utils/requests.ts
+++ frontend/src/utils/requests.ts
@@ -1,1 +1,6 @@
-export const BASE_URL = 'http://localhost:8080';
+export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8080';
+
+//Acima estou usando duas interrogações, chama-se operador de coalecencia nula.
+//significa que se esta variável não estiver definida no ambiente onde aplicação estiver rodando, 
+//por exemplo, ela nao esta definida, então se ela nao estiver definida eu vou pegar o valor 
+//que estiver a direita, dessa forma ira poder rodar localmente ou no netlify, se comunicando com o endereço do backend
